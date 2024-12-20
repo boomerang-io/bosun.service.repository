67daa24cca64e3f845779a7f456b8d90a52af2b8
Chummar Paily <chummar.paily@ibm.com>
2024-12-20 19:58:25 +0530

    Updated Spring boot version to 3.2.8

--- src/main/java/net/boomerangplatform/SecurityConfiguration.java
+++ src/main/java/net/boomerangplatform/SecurityConfiguration.java
@@ -1,15 +1,17 @@
 package net.boomerangplatform;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-  @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeHttpRequests(requests -> requests.requestMatchers("/").permitAll());
+    return httpSecurity.build();
   }
 
 }
