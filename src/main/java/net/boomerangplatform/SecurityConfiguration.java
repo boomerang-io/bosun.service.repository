f9e66a85220cd8567579ae83cb15c8f0a2af3ace
Chummar Paily <chummar.paily@ibm.com>
2025-05-09 23:09:31 +0530

    Updated web security to allow health check

--- src/main/java/net/boomerangplatform/SecurityConfiguration.java
+++ src/main/java/net/boomerangplatform/SecurityConfiguration.java
@@ -8,9 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 
+    private static final String API_DOCS = "/apis/docs/**";
+
+  private static final String HEALTH = "/health";
+
   @Bean
   SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeHttpRequests(requests -> requests.requestMatchers("/").permitAll());
+    httpSecurity.authorizeHttpRequests(requests -> requests.requestMatchers("/",HEALTH, API_DOCS).permitAll());
     return httpSecurity.build();
   }
 
